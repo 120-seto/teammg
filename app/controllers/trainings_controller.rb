af909ffafd910203587e5ba08b4d681c7693d667
120-seto <ks10c135@gmail.com>
2022-05-11 19:00:43 +0900

    練習メニューページへ遷移

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,6 +1,22 @@
 class TrainingsController < ApplicationController
   def index
+    @trainings = Training.all
   end
+
   def new
+    @trainings = Training.new
   end
+
+  def create
+    @trainings = Training.create(training_params)
+  end
+  
+  def show
+  end
+  private
+
+  def training_params
+    params.require(:training).permit(:all, :fw, :mf, :df, :gk, :list, :image).merge(user_id: current_user.id)
+  end
+
 end
