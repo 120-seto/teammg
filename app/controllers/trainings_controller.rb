151e97f4e4e5f70b06e32e112cc84a0390312e64
120-seto <ks10c135@gmail.com>
2022-05-12 16:38:16 +0900

    練習メニュー作成

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -3,4 +3,20 @@ class TrainingsController < ApplicationController
   end
   def new
   end
+<<<<<<< Updated upstream
+=======
+
+  def create
+    Training.create(training_params)
+  end
+  
+  def show
+  end
+  private
+
+  def training_params
+    params.require(:training).permit(:all, :fw, :mf, :df, :gk, :list, :image).merge(user_id: current_user.id)
+  end
+
+>>>>>>> Stashed changes
 end
