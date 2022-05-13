eff389b3d119453c4f74b2fbd5121284a1cebf2d
120-seto <ks10c135@gmail.com>
2022-05-13 19:03:34 +0900

    allモデル・コントローラー削除

--- app/models/all.rb
+++
@@ -1,7 +0,0 @@
-class All < ApplicationRecord
-  belongs_to :user
-  has_one :training
-  #has_one_attached :image
-
-  validates :all_text, presence: true
-end
--- db/migrate/20220513084316_create_alls.rb
+++
@@ -1,9 +0,0 @@
-class CreateAlls < ActiveRecord::Migration[6.0]
-  def change
-    create_table :alls do |t|
-      t.text     :all_text,     null: false
-      t.datetime :all_date
-      t.timestamps
-    end
-  end
-end
