ac2926849c48bb6296eb00792351a66eca3f868a
120-seto <ks10c135@gmail.com>
2022-05-13 23:05:40 +0900

    Active Storageの導入

--- app/controllers/totals_controller.rb
+++ app/controllers/totals_controller.rb
@@ -2,4 +2,10 @@ class TotalsController < ApplicationController
   def index
     @totals = Total.all
   end
+  private
+
+  def training_params
+    params.require(:total).permit(:total_title, :total_text, :total_date ,:image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/total.rb
+++ app/models/total.rb
@@ -1,7 +1,7 @@
 class Total < ApplicationRecord
   belongs_to :user
   has_one :training
-  #has_one_attached :image
+  has_one_attached :image
   validates :total_title, presence: true
   validates :total_text,  presence: true
 end
