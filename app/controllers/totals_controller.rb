75c34ff9242887e9add2a98b94d79a8283eb356f
120-seto <ks10c135@gmail.com>
2022-05-14 18:06:01 +0900

    Active Storageの導入-2

--- app/controllers/totals_controller.rb
+++ app/controllers/totals_controller.rb
@@ -1,7 +1,29 @@
 class TotalsController < ApplicationController
   def index
-    @totals = Total.all
+    @totals = Total.all.order(created_at: :desc)
   end
+
+  def new
+    @total = Total.new
+  end
+
+  def create
+    @total = Total.new(total_params)
+    if @total.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @total = Total.find(params[:id])
+  end
+
+  def edit
+    @total = Total.find(params[:id])
+  end
+
   private
 
   def training_params
