278a61a5315ee3bafac8c4ca7afeb372d54c144d
120-seto <ks10c135@gmail.com>
2022-05-20 23:20:28 +0900

    dfコントローラー作成

---
+++ app/controllers/dfs_controller.rb
@@ -0,0 +1,32 @@
+class DfsController < ApplicationController
+  def index
+    @dfs = Df.all.order(created_at: :desc)
+  end
+
+  def new
+    @df = Df.new
+  end
+
+  def create
+    @df = Df.new(df_params)
+    if @df.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @df = Df.find(params[:id])
+  end
+
+  def edit
+    @df = Df.find(params[:id])
+  end
+
+  private
+
+  def training_params
+    params.require(:df).permit(:df_title, :df_text, :df_date, :image).merge(user_id: current_user.id)
+  end
+end
