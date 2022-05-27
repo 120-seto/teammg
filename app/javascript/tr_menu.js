46934a1999c418c4a6147f6ac8ef3246edb75949
120-seto <ks10c135@gmail.com>
2022-05-27 12:05:18 +0900

    Jquery作成

--- app/javascript/tr_menu.js
+++ app/javascript/tr_menu.js
@@ -1,12 +1,11 @@
-window.addEventListener('load', function(){
-  const pullDownButton = document.getElementById("tr-menus")
-  pullDownButton.addEventListener('mouseover', function(){
-    pullDownButton.setAttribute("style", "background-color:red;")
-  })
-  pullDownButton.addEventListener('mouseout', function(){
-    pullDownButton.removeAttribute("style", "background-color:red;")
-  })
-  pullDownButton.addEventListener('click', function() {
-    console.log("click OK")
-  })
-})
+const main = document.querySelectorAll(".tr-image");
+const item = Array.prototype.slice.call(main,0);
+
+item.forEach(function (element) {
+  element.addEventListener("mouseover",function(){
+    element.querySelector(".tr").classList.add("open");
+  },false);
+  element.addEventListener("mouseout",function(){
+    element.querySelector(".tr").classList.remove("open");
+  },false);
+});
