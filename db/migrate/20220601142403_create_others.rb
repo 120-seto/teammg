c104aa160f39d4b93853772e67adbbf7ae153a9f
120-seto <ks10c135@gmail.com>
2022-06-04 18:36:06 +0900

    delカラムを削除

--- db/migrate/20220601142403_create_others.rb
+++ db/migrate/20220601142403_create_others.rb
@@ -4,7 +4,6 @@ class CreateOthers < ActiveRecord::Migration[6.0]
       t.text       :new
       t.text       :login
       t.text       :logout
-      t.text       :del
       t.references :user,             null: false, foreign_key: true
       t.timestamps
     end
